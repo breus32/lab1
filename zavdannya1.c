96f45021f831e562c704832d40b7ca2b9ff36d94
breus32 <muller.09@mail.ru>
2016-05-05 21:19:26 +0300

    Update zavdannya1.c

--- zavdannya1.c
+++ zavdannya1.c
@@ -4,16 +4,17 @@
 int main()
 
 {
-	int x, y, x1, y1;
+    int x, y, x1, y1;
+    
     printf("x=");
-	scanf("%d" ,&x);
-	y = x * x;
-	printf ("y=%d", y);
+    scanf("%d" ,&x);
+    y = x * x;
+    printf ("y=%d", y);
 	
     printf("\nx1=");
-	scanf("%d" ,&x1);
-	y1 = x1 * x1;
-	printf ("y1=%d", y1);
+    scanf("%d" ,&x1);
+    y1 = x1 * x1;
+    printf ("y1=%d", y1);
 
     return 0;
 }
