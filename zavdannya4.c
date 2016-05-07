a96b2832635dd6736bd143c30d6206ea31ac70bb
breus32 <muller.09@mail.ru>
2016-05-07 18:06:26 +0300

    Update zavdannya4.c

--- zavdannya4.c
+++ zavdannya4.c
@@ -3,12 +3,13 @@
 int main ()
 
 {
-	int a;
-	printf("a=");
-	scanf("%d", &a);
-	printf("\n%3.2d\n", a);
-	printf("\n%e\n", a);
-	printf("\n%3.7d\n", a);
-	
-	return 0;
+    int a;
+    printf("a=");
+    scanf("%d", &a);
+    
+    printf("\n%3.2d\n", a);
+    printf("\n%e\n", a);
+    printf("\n%3.7d\n", a);
+    
+    return 0;
 }
