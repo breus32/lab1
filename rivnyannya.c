9086962da5b812fbe37c3345f13ac6fd15f6b154
breus32 <muller.09@mail.ru>
2016-05-07 18:03:37 +0300

    Update rivnyannya.c

--- rivnyannya.c
+++ rivnyannya.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-
 int main()
 {
-        float x,y;
-        
-        printf("x=");
-        scanf("%f", &x);
-        
-        y = x * x;
-        printf("%3.2f",y);
-        
-        return 0;
+
+    float x,y;
+    
+    printf("x=");
+    scanf("%f", &x);
+    
+    y = x * x;
+    printf("%3.2f",y);
+    
+    return 0;
 }
