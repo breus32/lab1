7901e3ec2d98e6ef45986b912d6578b8319cc7cd
breus32 <muller.09@mail.ru>
2016-05-07 18:05:18 +0300

    Update zavdannya2.c

--- zavdannya2.c
+++ zavdannya2.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-
-int main() {
-	
-	double a,b,x;
-	
-	printf("Vvedite a:");
-	scanf("%f", &a);
-	
-	printf("Vvedite b:");
-	scanf("%f", &b);
-	
-	x = - b / a ;
-	
-	printf("x=%f", x); 
-	
-	return 0;
+int main()
+{
+    double a, b, x;
+    
+    printf("Vvedite a:");
+    scanf("%f", &a);
+    
+    printf("Vvedite b:");
+    scanf("%f", &b);
+    
+    x = - b / a ;
+    printf("x=%f", x);
+    
+    return 0;
 }
