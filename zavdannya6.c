227e0fe30500cdb69130fb1091d448993fe9b686
breus32 <muller.09@mail.ru>
2016-05-19 09:12:08 +0300

    Поміняти місцями дві змінні

---
+++ zavdannya6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	
+	printf("a: ");
+	scanf("%d", &a);
+	
+	printf("b: ");
+	scanf("%d", &b);
+	
+	a=a+b;
+    b=a-b;
+    a=a-b;
+	
+	printf("a = %d", a);
+	printf("\nb = %d", b);
+	
+	return 0;
+}
